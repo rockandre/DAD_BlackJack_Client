b404573ca3ef0c2a44920bacf6bf4c9b2021e501
André Marques Roque <rockandre31@gmail.com>
2018-02-01 13:25:29 +0000

    final

--- app/Http/Controllers/LoginControllerAPI.php
+++ app/Http/Controllers/LoginControllerAPI.php
@@ -27,124 +27,125 @@ class LoginControllerAPI extends Controller
 
     use SendsPasswordResetEmails;
 
-	public function login(Request $request)
-	{
+    public function login(Request $request)
+    {
         $user = User::orWhere('email', $request->email)->orWhere('nickname', $request->email)->first();
         if(!$user){
             return response()->json(['msg'=>'User/Email dont exist'], 400);
-        }
-
-        if($user->activated == 0){
-            return response()->json(['msg'=>'User not active.'], 400);
-        }
-
-        if($user->blocked == 1){
-            return response()->json(['msg'=>'User blocked.'], 400);
-        }
-
-		$http = new \GuzzleHttp\Client;
-		$response = $http->post(YOUR_SERVER_URL.'/oauth/token', [
-			'form_params' => [
-				'grant_type' => 'password',
-				'client_id' => CLIENT_ID,
-				'client_secret' => CLIENT_SECRET,
-				'username' => $request->email,
-				'password' => $request->password,
-				'scope' => ''
-			],
-			'exceptions' => false,
-		]);
-		$errorCode= $response->getStatusCode();
-
-		if ($errorCode=='200') {
-			return json_decode((string) $response->getBody(), true);
-		} else {
-			return response()->json(
-				['msg'=>'User credentials are invalid'], $errorCode);
-		}
-	}
-
-	public function logout()
-	{
-		\Auth::guard('api')->user()->token()->revoke();
-		\Auth::guard('api')->user()->token()->delete();
-		return response()->json(['msg'=>'Token revoked'], 200);
-	}
-
-	public function sendEmail(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email'
-        ]);
-
-
-        if ($request->wantsJson() && !$validator->fails()) {
-            $user = User::where('email', $request->input('email'))->first();
-            if (!$user) {
-                return response()->json(['data' => trans('passwords.user')], 400);
-            }
-
-            $token = $this->broker()->createToken($user);
-
-            try{
-
-                $config = DB::table('config')->first();
-                $mailConfigs = json_decode($config->platform_email_properties);
-                config([
-                   'mail.host' => $mailConfigs->host,
-                   'mail.port' => $mailConfigs->port,
-                   'mail.encryption' =>$mailConfigs->encryption,
-                   'mail.username' => $config->platform_email,
-                   'mail.password' => $mailConfigs->password
-               ]);
-
-                $app = App::getInstance();
-                $app->singleton('swift.transport', function ($app) {
-                    return new TransportManager($app);
-                });
-                $mailer = new Swift_Mailer($app['swift.transport']->driver());
-                Mail::setSwiftMailer($mailer);
-
-                Mail::to($user->email)->send(new resetlink($token, $config->platform_email));
-                return response()->json(['data' => 'Email sended.']);
-            }
-            catch(\Exception $e){
-                return response()->json(['data' => 'Problems sending email: '.$e], 400);
-            }
         } else {
-            return response()->json(['data' => 'Invalid request.'], 400);
-        }
-    }
-
-    public function resetPassword(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'token' => 'required',
-            'password' => 'required'
-        ]);
-
-        if ($request->wantsJson() && !$validator->fails()) {
-            
-            $user = User::where('email', $request->input('email'))->first();
-            if (!$user) {
-                return response()->json(['msg' => trans('passwords.user')], 400);
+            if($user->activated == 0){
+                return response()->json(['msg'=>'User not active.'], 400);
             }
 
-            $reminder = DB::table('password_resets')->where('email', $user->email)->first();
-            if (!$reminder or !Hash::check($request->input('token'), $reminder->token)) {
-                return response()->json(['data' => 'Token valid.'], 400);
+            if($user->blocked == 1){
+                return response()->json(['msg'=>'User blocked.'], 400);
             }
-
-            $user->password = Hash::make($request->input('password'));
-            $user->save();
-
-            DB::table('password_resets')->where('email', $user->email)->delete();
-
-            return response()->json(['data' => 'Password changed.'], 200);
-        } else {
-            return response()->json(['data' => 'Invalid request.'], 400);
         }
+
+
+        $http = new \GuzzleHttp\Client;
+        $response = $http->post(YOUR_SERVER_URL.'/oauth/token', [
+         'form_params' => [
+            'grant_type' => 'password',
+            'client_id' => CLIENT_ID,
+            'client_secret' => CLIENT_SECRET,
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => ''
+        ],
+        'exceptions' => false,
+    ]);
+        $errorCode= $response->getStatusCode();
+
+        if ($errorCode=='200') {
+         return json_decode((string) $response->getBody(), true);
+     } else {
+         return response()->json(
+            ['msg'=>'User credentials are invalid'], $errorCode);
+     }
+ }
+
+ public function logout()
+ {
+  \Auth::guard('api')->user()->token()->revoke();
+  \Auth::guard('api')->user()->token()->delete();
+  return response()->json(['msg'=>'Token revoked'], 200);
+}
+
+public function sendEmail(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|email'
+    ]);
+
+
+    if ($request->wantsJson() && !$validator->fails()) {
+        $user = User::where('email', $request->input('email'))->first();
+        if (!$user) {
+            return response()->json(['data' => trans('passwords.user')], 400);
+        }
+
+        $token = $this->broker()->createToken($user);
+
+        try{
+
+            $config = DB::table('config')->first();
+            $mailConfigs = json_decode($config->platform_email_properties);
+            config([
+             'mail.host' => $mailConfigs->host,
+             'mail.port' => $mailConfigs->port,
+             'mail.encryption' =>$mailConfigs->encryption,
+             'mail.username' => $config->platform_email,
+             'mail.password' => $mailConfigs->password
+         ]);
+
+            $app = App::getInstance();
+            $app->singleton('swift.transport', function ($app) {
+                return new TransportManager($app);
+            });
+            $mailer = new Swift_Mailer($app['swift.transport']->driver());
+            Mail::setSwiftMailer($mailer);
+
+            Mail::to($user->email)->send(new resetlink($token, $config->platform_email));
+            return response()->json(['data' => 'Email sended.']);
+        }
+        catch(\Exception $e){
+            return response()->json(['data' => 'Problems sending email: '.$e], 400);
+        }
+    } else {
+        return response()->json(['data' => 'Invalid request.'], 400);
     }
+}
+
+public function resetPassword(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|email',
+        'token' => 'required',
+        'password' => 'required'
+    ]);
+
+    if ($request->wantsJson() && !$validator->fails()) {
+
+        $user = User::where('email', $request->input('email'))->first();
+        if (!$user) {
+            return response()->json(['msg' => trans('passwords.user')], 400);
+        }
+
+        $reminder = DB::table('password_resets')->where('email', $user->email)->first();
+        if (!$reminder or !Hash::check($request->input('token'), $reminder->token)) {
+            return response()->json(['data' => 'Token valid.'], 400);
+        }
+
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        DB::table('password_resets')->where('email', $user->email)->delete();
+
+        return response()->json(['data' => 'Password changed.'], 200);
+    } else {
+        return response()->json(['data' => 'Invalid request.'], 400);
+    }
+}
 
 }
